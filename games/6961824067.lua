9ab2f5ddc96cffeff2817a2ea9f61245a883e9b2
GameSTALkER <52292220+GameSTALkER@users.noreply.github.com>
2022-04-09 22:28:52 +0200

    Fling Things and People

--- games/6961824067.lua
+++ games/6961824067.lua
@@ -4,6 +4,7 @@ local gui = ngstloader:AddMenu("FTaP")
 gui = gui:AddTab("General")
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
+local plrs = game:GetService("Players")
 
 local throw = {}
 
@@ -37,16 +38,49 @@ function Int()
     end
 end
 
-Int()
-LocalPlayer.CharacterAdded:Connect(function()
-    repeat wait() until LocalPlayer.Character
-    repeat wait() until LocalPlayer.Character:FindFirstChildWhichIsA("Humanoid")
-    repeat wait() until LocalPlayer.Character:FindFirstChild("GrabbingScript")
-    Int()
-end)
-
+gui:CreateButton({name="Activate Throw power hack"}, function() Int() end)
 gui:CreateSlider({name="Throw power", min=750, def=throw_limit, max=10000}, function(_)
     throw_limit = _
     debug.setconstant(throw[1], throw[2], throw_limit)
 
+end)
+local isserverlagging = false
+gui:CreateToggle({name="Lag players (press multiple times)", desc="Lag players(you don't get lags)\nRejoin to remove lag"}, function(_)
+    isserverlagging = _
+    if _ then
+        spawn(function()
+            for i,v in pairs(game.workspace:GetDescendants()) do
+                game:GetService("ReplicatedStorage").CharacterEvents.Beam:FireServer(unpack({[1] = "make",[2] = v,[3] = Vector3.new(0,0,0)}))
+                wait()
+                if not isserverlagging then break end
+            end
+        end)
+    else
+        spawn(function()
+            for i,v in pairs(game.workspace:GetDescendants()) do
+                game:GetService("ReplicatedStorage").CharacterEvents.Beam:FireServer(unpack({[1] = "destroy",[2] = v}))
+                wait()
+                if isserverlagging then break end
+            end
+        end)
+    end
+end)
+getgenv().getgoodgetlaydog = false
+gui:CreateToggle({name="Make everyone around you lay", desc="Really funny :)"},function(_)
+    getgenv().getgoodgetlaydog = _
+    if getgenv().getgoodgetlaydog then 
+        spawn(function()
+            while getgenv().getgoodgetlaydog do
+                for i,v in pairs(plrs:GetChildren()) do
+                    pcall(function()
+                        if v.Name ~= plrs.LocalPlayer.Name then
+                            game:GetService("ReplicatedStorage").GrabEvents.SetNetworkOwner:FireServer(unpack({[1] = v.Character.HumanoidRootPart,[2] = "player"}))
+                        end
+                    end)
+                    wait()
+                end
+                wait()
+            end
+        end)
+    end
 end)
