6c676369d87c574ef6fc2a509596a58656ef36d0
GameSTALkER <52292220+GameSTALkER@users.noreply.github.com>
2022-05-18 12:07:15 -0700

    Ragdoll Engine

--- games/8892207911.lua
+++ games/8892207911.lua
@@ -3,17 +3,34 @@
 local menu = getgenv().ngstloader:AddMenu("Ragdoll Engine")
 local tab1 = menu:AddTab("General")
 
+btn = {[1]=nil,[2]=nil}
+
 local plr = game:GetService("Players").LocalPlayer
 
-tab1:CreateToggle({name="Toggle Ragdoll",exec=true},function(state)
-    for i,v in pairs(plr.Character:GetChildren()) do
-        if (v.ClassName == "LocalScript" or v.ClassName == "Script") and table.find({"Falling down","READ ME","Swimming","StartRagdoll","Pushed","KillScript","RagdollMe"},v.Name) then 
-            v.Disabled = not state
-            
-        end
-        
+btn[1] = tab1:CreateToggle({name="Toggle Ragdoll",exec=true},function(state)
+    local rg = plr.Character:FindFirstChild("RagdollMe")
+    local rg1 = plr.Character:FindFirstChild("StartRagdoll")
+    
+    if rg ~= nil and rg1 ~= nil then
+        rg.Disabled = not state
+        rg1.Disabled = not state
+    else
+        btn[1]:Change({name="Reset character",state=false})
+        wait(1)
+        btn[1]:Change({name="Toggle Ragdoll"})
     end
     
+end)
+btn[2] = tab1:CreateToggle({name="Toggle FirstPerson",exec=true}, function(state)
+    local fp = plr.Character:FindFirstChild("FirstPerson")
+    
+    if fp ~= nil then
+        fp.Disabled = not state
+    else
+        btn[2]:Change({name="Reset character",state=false})
+        wait(1)
+        btn[2]:Change({name="Toggle FirstPerson"})
+    end
 end)
 tab1:CreateSlider({name = "WalkSpeed",min=1,def=16},function(_)
     plr.Character.Humanoid.WalkSpeed = _
