5b735a6b2771b9b3cb3bfeef91489d476b78aba1
GameSTALkER <52292220+GameSTALkER@users.noreply.github.com>
2022-06-18 03:52:45 -0700

    Update UtilLib.lua

--- UtilLib.lua
+++ UtilLib.lua
@@ -97,3 +97,120 @@ getgenv().CFG = function(location, action)
     end
 
 end
+
+local default_settings = {
+    -- core settings
+    debug = true; -- print information in console
+    bloxify = false; -- from normal UGS to gray block (R6 only)
+    speed = 100;
+    
+    pos = Vector3.new(0,0,0);
+    rot = Vector3.new(0,0,0);
+}
+getgenv().Accessory = function(hatName,parent,settings)
+    
+    local default_settings = {
+        -- core settings
+        debug = true; -- print information in console
+        bloxify = false; -- from normal UGS to gray block (R6 only)
+        speed = 100;
+        
+        pos = Vector3.new(0,0,0);
+        rot = Vector3.new(0,0,0);
+    }
+    local function neededType(value,ntype)
+        if type(value) == ntype then 
+            return true 
+        elseif rstr == nil then 
+            return false 
+        end
+    end
+    
+    if hatName == nil then print("hatName can't be nil.");return nil end
+    if parent == nil then print("parent can't be nil.");return nil end
+    if type(settings) ~= "table" then 
+        print("loaded default settings.")
+        settings = default_settings
+    else
+        for i,v in pairs(settings) do
+            if (i:lower() == "debug") and neededType(v,"boolean") then 
+                default_settings.debug = v
+            elseif (i:lower() == "bloxify" or i:lower() == "mesh") and neededType(v,"boolean") then
+                default_settings.bloxify = v
+            elseif (i:lower() == "speed") and neededType(v,"number") then 
+                default_settings.speed = v
+            elseif (i:lower() == "pos" or i:lower() == "position" or i:lower() == "p") and neededType(v,"vector") then 
+                default_settings.pos = v
+            elseif (i:lower() == "rot" or i:lower() == "rotation" or i:lower() == "r") and neededType(v,"vector") then 
+                default_settings.rot = v
+            else print(i.."="..tostring(v).."("..type(v)..") is unknown or has incorrect value type.")
+            end
+        end
+        settings = default_settings
+    end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/GameSTALkER/ngstloader/main/scripts/AnimHub.Scripts/Universal/netless.lua"))()
+    local function debug(str) if settings.debug then print(tostring(str)) end end
+    
+    local Player = game:GetService("Players").LocalPlayer
+    local Character = Player.Character
+    
+    local hat = nil
+    if getgenv().hats_attributes == nil then getgenv().hats_attributes = {} end
+    for i,v in pairs(Character.Humanoid:GetAccessories()) do
+        if v.Name == hatName then hat = v end
+    end
+    if hat == nil then print("Hat not found."); return nil end
+    pcall(function() hat.Handle.AccessoryWeld:Destroy() end)
+    if settings.bloxify then
+        for _,mesh in pairs(hat:GetDescendants()) do
+            if mesh:IsA("Mesh") or mesh:IsA("SpecialMesh") then
+                mesh:Remove()
+            end
+        end
+    end
+    if hat:GetAttribute("IsReanimated") == true and getgenv().hats_attributes[hat.Name] then
+        getgenv().hats_attributes[hat.Name].att1.Parent = parent
+        getgenv().hats_attributes[hat.Name].att1.Position = settings.pos
+        getgenv().hats_attributes[hat.Name].att1.Rotation = settings.rot
+        getgenv().hats_attributes[hat.Name].Speed1.Responsiveness = settings.speed
+        getgenv().hats_attributes[hat.Name].Speed2.Responsiveness = settings.speed
+    else
+        getgenv().hats_attributes[hat.Name] = {att0=nil,att1=nil,Speed1=nil,Speed2=nil}
+        -- Handle parent
+        local att0 = Instance.new("Attachment", hat.Handle) -- hat att
+        att0.Position = Vector3.new(0,0,0)
+        getgenv().hats_attributes[hat.Name].att0 = att0
+        
+        local att1 = Instance.new("Attachment", parent) -- parent to
+        att1.Name = hat.Name
+        att1.Position = settings.pos
+        att1.Rotation = settings.rot
+        getgenv().hats_attributes[hat.Name].att1 = att1
+        
+        -- Handle parent
+        local AP = Instance.new("AlignPosition", hat.Handle)
+        AP.Attachment0 = att0
+        AP.Attachment1 = att1
+        AP.RigidityEnabled = false
+        AP.ReactionForceEnabled = false
+        AP.ApplyAtCenterOfMass = true
+        AP.MaxForce = math.huge
+        AP.MaxVelocity = math.huge
+        AP.Responsiveness = settings.speed
+        getgenv().hats_attributes[hat.Name].Speed1 = AP
+        
+        local AO = Instance.new("AlignOrientation", hat.Handle)
+        AO.Attachment0 = att0
+        AO.Attachment1 = att1
+        AO.ReactionTorqueEnabled = false
+        AO.PrimaryAxisOnly = false
+        AO.MaxTorque = math.huge
+        AO.MaxAngularVelocity = math.huge
+        AO.Responsiveness = settings.speed
+        getgenv().hats_attributes[hat.Name].Speed2 = AO
+        
+        hat:SetAttribute("IsReanimated",true)
+    end
+    return hat
+    
+end
